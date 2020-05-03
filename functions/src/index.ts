e4034c873a96ded81ce74dad37508ba8a6f8b4cc
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-05-03 20:56:07 +0800

    Ran linter and formatter.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,31 +1,33 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-// import * as algoliasearch from 'algoliasearch';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 const algoliasearch = require("algoliasearch");
-const env = functions.config()
+
+const env = functions.config();
 admin.initializeApp();
 
-const client = algoliasearch(env.algolia.appid ,env.algolia.apikey);
-const index = client.initIndex('robotcone');
+const client = algoliasearch(env.algolia.appid, env.algolia.apikey);
+const index = client.initIndex("robotcone");
 
-exports.UpdateProduct = 
-functions.firestore.document("product/{productID}").onUpdate(change => {
+exports.UpdateProduct = functions.firestore
+  .document("product/{productID}")
+  .onUpdate((change) => {
     const data = change.after.data();
     const objectID = change.after.id;
 
     return index.saveObject({
-        objectID,
-        data
+      objectID,
+      data,
     });
-});
+  });
 
-exports.CreateProduct = 
-functions.firestore.document("product/{productID}").onCreate((snapshot , context) => {
+exports.CreateProduct = functions.firestore
+  .document("product/{productID}")
+  .onCreate((snapshot, context) => {
     const data = snapshot.data();
     const objectID = snapshot.id;
 
     return index.saveObject({
-        objectID,
-        data
+      objectID,
+      data,
     });
-});
+  });
